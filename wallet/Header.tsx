00f682df5ca0d43298a3b14781c4ba305be01819
evgpan <cuiguo23@gmail.com>
2024-02-12 10:18:30 +0000

    feat: complete

---
+++ wallet/Header.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react"
+import { WalletMultiButton } from "./WalletMultiButton"
+
+export default function({
+  children
+} : {
+  children?: ReactNode
+}) {
+  return (
+    <WalletMultiButton />
+  )
+}
